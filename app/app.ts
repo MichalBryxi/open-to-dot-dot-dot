8869dd3b7395cf3bf700511b768c6085f0b0376b
Michal Bryxí <michal.bryxi@gmail.com>
2024-09-22 22:49:23 +0200

    fix: Add basic OpenTo component

--- app/app.ts
+++ app/app.ts
@@ -2,8 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'open-to-dot-dot-dot/config/environment';
-
-import './styles/app.css'
+import './styles/app.css';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
